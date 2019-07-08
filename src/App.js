0b8f2b556d2adbd9ba11ab84089dd5a419798c54
muturi254 <petermuturi74@gmail.com>
2019-07-08 20:45:54 +0300

    Created components folder to hold compnents, then respectively created component folder with the repective files updated App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { fab } from '@fortawesome/free-brands-svg-icons';
+// import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './App.scss';
 
 // adding icons to the library
-library.add(fab, faCheckSquare, faCoffee)
+// library.add(fab, faCheckSquare, faCoffee)
 // end of library 
 
 function App() {
   return (
    <div>
-      <h1>Hello from App <span><FontAwesomeIcon icon={['fab', 'google']} /></span></h1>
+      {/* <FontAwesomeIcon icon={['fab', 'google']} /> */}
+      <h1>Hello from App <span></span></h1>
       
    </div>
   );
