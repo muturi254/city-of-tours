a2c264b72c7a0367298108c116cd1be2c725ff44
muturi254 <petermuturi74@gmail.com>
2019-07-09 20:01:14 +0300

    corrected bugs

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -8,13 +8,22 @@ class TourList extends React.Component {
     state = {
         tours: tourData
     }
+    // method to delete tour component
+    removeTour = (id) => {
+        const {tours} = this.state;
+        const sortedTours = tours.filter(tour=> tour.id !== id);
+        this.setState({
+            tours: sortedTours
+        })
+    }
+
     render() {
         const { tours } = this.state;
         return (
             <section className="tourlist">
                 {tours.map(tour => {
                     return (
-                        <Tour key={tour.id} tour={tour} />                        
+                        <Tour key={tour.id} tour={tour} removeTour={ this.removeTour } />                        
                     );
                 })}
             </section>
